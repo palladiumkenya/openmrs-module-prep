e96c37d10c2b969268e35c25485a3f3440291c14
njorocs <njorocs@gmail.com>
2019-04-17 18:27:20 +0300

    Configured module depedencies

--- api/src/main/java/org/openmrs/module/prep/PrepActivator.java
+++ api/src/main/java/org/openmrs/module/prep/PrepActivator.java
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
+ * <p>
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
@@ -11,27 +11,55 @@ package org.openmrs.module.prep;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.ModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or shutdown
  */
-public class PrepActivator extends BaseModuleActivator {
+public class PrepActivator implements ModuleActivator {
 	
 	private Log log = LogFactory.getLog(this.getClass());
 	
 	/**
-	 * @see #started()
+	 * @see ModuleActivator#willRefreshContext()
 	 */
-	public void started() {
-		log.info("Started Prep");
+	public void willRefreshContext() {
+		log.info("Refreshing Prep in KenyaEMR Module");
 	}
 	
 	/**
-	 * @see #shutdown()
+	 * @see ModuleActivator#contextRefreshed()
 	 */
-	public void shutdown() {
-		log.info("Shutdown Prep");
+	public void contextRefreshed() {
+		log.info("Prep in KenyaEMR Module refreshed");
+	}
+	
+	/**
+	 * @see ModuleActivator#willStart()
+	 */
+	public void willStart() {
+		log.info("Starting Prep in KenyaEMR Module");
+	}
+	
+	/**
+	 * @see ModuleActivator#started()
+	 */
+	public void started() {
+		log.info("Prep in KenyaEMR Module started");
+	}
+	
+	/**
+	 * @see ModuleActivator#willStop()
+	 */
+	public void willStop() {
+		log.info("Stopping Prep in KenyaEMR Module");
+	}
+	
+	/**
+	 * @see ModuleActivator#stopped()
+	 */
+	public void stopped() {
+		log.info("Prep in KenyaEMR Module stopped");
 	}
 	
 }
--- omod/src/main/java/org/openmrs/module/prep/web/controller/PrepController.java
+++ omod/src/main/java/org/openmrs/module/prep/web/controller/PrepController.java
@@ -9,18 +9,11 @@
  */
 package org.openmrs.module.prep.web.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.User;
-import org.openmrs.api.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openmrs.api.context.Context;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,60 +21,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * This class configured as controller using annotation and mapped with the URL of
  * 'module/${rootArtifactid}/${rootArtifactid}Link.form'.
  */
-@Controller("${rootrootArtifactid}.PrepController")
-@RequestMapping(value = "module/${rootArtifactid}/${rootArtifactid}.form")
+@Controller
+
 public class PrepController {
-	
-	/** Logger for this class and subclasses */
 	protected final Log log = LogFactory.getLog(getClass());
-	
-	@Autowired
-	UserService userService;
-	
-	/** Success form view name */
-	private final String VIEW = "/module/${rootArtifactid}/${rootArtifactid}";
-	
-	/**
-	 * Initially called after the getUsers method to get the landing form name
-	 * 
-	 * @return String form view name
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public String onGet() {
-		return VIEW;
+	@RequestMapping(value = "/module/org.openmrs.module.prep/prep",method = RequestMethod.GET)
+	public void manage(ModelMap model){
+		model.addAttribute("user", Context.getAuthenticatedUser());
 	}
-	
-	/**
-	 * All the parameters are optional based on the necessity
-	 * 
-	 * @param httpSession
-	 * @param anyRequestObject
-	 * @param errors
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public String onPost(HttpSession httpSession, @ModelAttribute("anyRequestObject") Object anyRequestObject,
-	        BindingResult errors) {
-		
-		if (errors.hasErrors()) {
-			// return error view
-		}
-		
-		return VIEW;
-	}
-	
-	/**
-	 * This class returns the form backing object. This can be a string, a boolean, or a normal java
-	 * pojo. The bean name defined in the ModelAttribute annotation and the type can be just defined
-	 * by the return type of this method
-	 */
-	@ModelAttribute("users")
-	protected List<User> getUsers() throws Exception {
-		List<User> users = userService.getAllUsers();
-		
-		// this object will be made available to the jsp page under the variable name
-		// that is defined in the @ModuleAttribute tag
-		return users;
-	}
-	
+
+
 }
