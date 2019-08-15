f66b42e35b09ac94bff6bbcb448f7770dad569dc
njorocs <njorocs@gmail.com>
2019-08-15 17:05:34 +0300

    Report definitions for PrEP cohort. Cohort queries

---
+++ api/src/main/java/org/openmrs/module/prep/reporting/data/converter/definition/evaluator/prep/HBVAgDataEvaluator.java
@@ -0,0 +1,48 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.prep.reporting.data.converter.definition.evaluator.prep;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.prep.reporting.data.converter.definition.prep.HBVAgDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * Evaluates PersonDataDefinition
+ */
+@Handler(supports = HBVAgDataDefinition.class, order = 50)
+public class HBVAgDataEvaluator implements PersonDataEvaluator {
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "select e.patient_id,mid(max(concat(l.visit_date,(case l.test_result when 665 then \"Negative\" when 703 then \"Positive\" when 1133 then \"Indeterminate\" else \"Not Done\" end))),11) as HBV_status from kenyaemr_etl.etl_prep_enrolment e left outer join\n" +
+				"       kenyaemr_etl.etl_laboratory_extract l on e.patient_id = l.patient_id\n" +
+				"where l.lab_test in (161472,159430) group by e.patient_id;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		queryBuilder.append(qry);
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
+}
