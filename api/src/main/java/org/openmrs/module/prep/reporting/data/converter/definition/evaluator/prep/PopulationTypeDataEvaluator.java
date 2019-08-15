14d2841f2d1a5f19812733874fded5d1b8844116
njorocs <njorocs@gmail.com>
2019-08-15 17:05:34 +0300

    Report definitions for PrEP cohort. Cohort queries

---
+++ api/src/main/java/org/openmrs/module/prep/reporting/data/converter/definition/evaluator/prep/PopulationTypeDataEvaluator.java
@@ -0,0 +1,48 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.prep.reporting.data.converter.definition.evaluator.prep;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.prep.reporting.data.converter.definition.prep.PopulationTypeDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * Evaluates PersonDataDefinition
+ */
+@Handler(supports = PopulationTypeDataDefinition.class, order = 50)
+public class PopulationTypeDataEvaluator implements PersonDataEvaluator {
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "select e.patient_id,case t.population_type when \"Discordant couple\" then 1 when \"Fisher Folk\" then 2\n" +
+				"    when \"General Population\" then 3 when \"Key Population\" then 4 else 5 end as population_type from kenyaemr_etl.etl_prep_enrolment e left outer join kenyaemr_etl.etl_hts_test t\n" +
+				"on e.patient_id = t.patient_id group by e.patient_id;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		queryBuilder.append(qry);
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
+}
