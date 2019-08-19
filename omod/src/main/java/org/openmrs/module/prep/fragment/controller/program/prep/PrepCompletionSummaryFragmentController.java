6e3def907f146e1dc0766c1eb330a75756ab9286
makombe <kennedymakombe@gmail.com>
2019-08-19 13:28:44 +0300

    Show program history snapshot for prep program

--- omod/src/main/java/org/openmrs/module/prep/fragment/controller/program/prep/PrepCompletionSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/prep/fragment/controller/program/prep/PrepCompletionSummaryFragmentController.java
@@ -10,37 +10,43 @@
 package org.openmrs.module.prep.fragment.controller.program.prep;
 
 import org.openmrs.Encounter;
-import org.openmrs.Form;
+import org.openmrs.Obs;
 import org.openmrs.PatientProgram;
-import org.openmrs.module.kenyacore.program.ProgramDescriptor;
-import org.openmrs.module.kenyacore.program.ProgramManager;
-import org.openmrs.module.prep.EmrWebConstants;
-import org.openmrs.module.prep.wrapper.Enrollment;
+import org.openmrs.module.prep.wrapper.EncounterWrapper;
 import org.openmrs.ui.framework.annotation.FragmentParam;
-import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.openmrs.api.context.Context;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Prep Program completion fragment
  */
 public class PrepCompletionSummaryFragmentController {
 	
-	public void controller(@FragmentParam("patientProgram") PatientProgram patientProgram,
-	        @FragmentParam("showClinicalData") boolean showClinicalData, @SpringBean ProgramManager programManager,
-	        FragmentModel model) {
+	public String controller(@FragmentParam("patientProgram") PatientProgram enrollment,
+	        @FragmentParam("showClinicalData") boolean showClinicalData,
+	        @FragmentParam(value = "encounter", required = false) Encounter encounter, FragmentModel model) {
+		Map<String, Object> dataPoints = new LinkedHashMap<String, Object>();
 		
-		ProgramDescriptor programDescriptor = programManager.getProgramDescriptor(patientProgram.getProgram());
-		Form defaultCompletionForm = programDescriptor.getDefaultCompletionForm().getTarget();
+		dataPoints.put("Completed", enrollment.getDateCompleted());
 		
-		Enrollment enrollment = new Enrollment(patientProgram);
+		if (showClinicalData && enrollment.getOutcome() != null) {
+			dataPoints.put("Outcome", enrollment.getOutcome());
+		}
 		
-		// Might not be the default completion form, but should have the same encounter type
-		Encounter encounter = enrollment.lastEncounter(defaultCompletionForm.getEncounterType());
+		if (encounter != null) {
+			EncounterWrapper wrapper = new EncounterWrapper(encounter);
+			
+			Obs reasonObs = wrapper.firstObs(Context.getConceptService().getConcept(161555));
+			if (reasonObs != null) {
+				dataPoints.put("Reason", reasonObs.getValueCoded());
+			}
+		}
 		
-		model.put("summaryFragment",
-		    programDescriptor.getFragments().get(EmrWebConstants.PROGRAM_COMPLETION_SUMMARY_FRAGMENT));
-		model.put("enrollment", patientProgram);
-		model.put("encounter", encounter);
-		model.put("showClinicalData", showClinicalData);
+		model.put("dataPoints", dataPoints);
+		return "view/dataPoints";
+		
 	}
 }
--- omod/src/main/java/org/openmrs/module/prep/fragment/controller/program/prep/PrepEnrollmentSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/prep/fragment/controller/program/prep/PrepEnrollmentSummaryFragmentController.java
@@ -10,37 +10,38 @@
 package org.openmrs.module.prep.fragment.controller.program.prep;
 
 import org.openmrs.Encounter;
-import org.openmrs.Form;
+import org.openmrs.Obs;
 import org.openmrs.PatientProgram;
-import org.openmrs.module.kenyacore.program.ProgramDescriptor;
-import org.openmrs.module.kenyacore.program.ProgramManager;
-import org.openmrs.module.prep.EmrWebConstants;
-import org.openmrs.module.prep.wrapper.Enrollment;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.prep.wrapper.EncounterWrapper;
 import org.openmrs.ui.framework.annotation.FragmentParam;
-import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Prep Program enrollment fragment
  */
 public class PrepEnrollmentSummaryFragmentController {
 	
-	public void controller(@FragmentParam("patientProgram") PatientProgram patientProgram,
-	        @FragmentParam("showClinicalData") boolean showClinicalData, @SpringBean ProgramManager programManager,
-	        FragmentModel model) {
+	public String controller(@FragmentParam("patientProgram") PatientProgram patientProgram,
+	        @FragmentParam("showClinicalData") boolean showClinicalData,
+	        @FragmentParam(value = "encounter", required = false) Encounter encounter, FragmentModel model) {
 		
-		ProgramDescriptor programDescriptor = programManager.getProgramDescriptor(patientProgram.getProgram());
-		Form defaultEnrollmentForm = programDescriptor.getDefaultEnrollmentForm().getTarget();
+		Map<String, Object> dataPoints = new LinkedHashMap<String, Object>();
+		dataPoints.put("Enrolled", patientProgram.getDateEnrolled());
 		
-		Enrollment enrollment = new Enrollment(patientProgram);
+		if (encounter != null) {
+			EncounterWrapper wrapper = new EncounterWrapper(encounter);
+			
+			Obs o = wrapper.firstObs(Context.getConceptService().getConcept(164932));
+			if (o != null) {
+				dataPoints.put("Entry point", o.getValueCoded());
+			}
+		}
 		
-		// Might not be the default enrollment form, but should have the same encounter type
-		Encounter encounter = enrollment.lastEncounter(defaultEnrollmentForm.getEncounterType());
-		
-		model.put("summaryFragment",
-		    programDescriptor.getFragments().get(EmrWebConstants.PROGRAM_ENROLLMENT_SUMMARY_FRAGMENT));
-		model.put("enrollment", patientProgram);
-		model.put("encounter", encounter);
-		model.put("showClinicalData", showClinicalData);
+		model.put("dataPoints", dataPoints);
+		return "view/dataPoints";
 	}
 }
