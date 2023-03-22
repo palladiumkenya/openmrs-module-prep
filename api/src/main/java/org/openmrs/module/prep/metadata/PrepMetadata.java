d9dd4ac1a4cc1f717b9425bc16c8f7ab8ff15193
gabriel090 <gabriel.mbatha@gmail.com>
2023-03-22 08:07:10 +0300

    Revise the PrEP weight eligibility category from 35 kgs to 30 kgs

--- api/src/main/java/org/openmrs/module/prep/metadata/PrepMetadata.java
+++ api/src/main/java/org/openmrs/module/prep/metadata/PrepMetadata.java
@@ -182,7 +182,7 @@ public class PrepMetadata extends AbstractMetadataBundle {
 		install(program("PrEP", "Pre exposure prophylaxis program", _Concept.PREP, _Program.PREP));
 		
 		// PrEP global properties
-		install(globalProperty("prep.weight", "Weight limit for one to be eligible for PrEP", "35"));
+		install(globalProperty("prep.weight", "Weight limit for one to be eligible for PrEP", "30"));
 		install(globalProperty("prep.age", "Age limit for one to be eligible for PrEP", "15"));
 		install(globalProperty("prep.htsInitialPeriod", "Time limit in days for hts to be valid for initiation into PrEP",
 		    "3"));
