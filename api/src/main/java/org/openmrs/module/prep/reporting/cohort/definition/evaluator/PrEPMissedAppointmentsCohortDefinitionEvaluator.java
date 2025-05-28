2913a14169123af0aa328754a284c77f7dafe1a4
njorocs <njorocs@gmail.com>
2025-05-28 16:12:35 +0300

    KHP3-8047: Fixing PrEP cohort missed appointment threshold. Fixed next appointment date not populating

--- api/src/main/java/org/openmrs/module/prep/reporting/cohort/definition/evaluator/PrEPMissedAppointmentsCohortDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/prep/reporting/cohort/definition/evaluator/PrEPMissedAppointmentsCohortDefinitionEvaluator.java
@@ -45,25 +45,25 @@ public class PrEPMissedAppointmentsCohortDefinitionEvaluator implements CohortDe
 			return null;
 		
 		Cohort newCohort = new Cohort();
-		String qry = "select  m.patient_id\n" + " from (\n"
-		        + "   select f.patient_id, latest_vis_date, latest_tca, date_discontinued, disc_patient\n" + "   from (\n"
-		        + "   select t.patient_id,\n" + "    mid(max(concat(t.visit_date, t.appointment_date)), 11) latest_tca,\n"
-		        + "    max(t.visit_date) as latest_vis_date,\n" + "    d.patient_id      as disc_patient,\n"
-		        + "    max(d.visit_date) as date_discontinued\n" + "    from (\n"
-		        + "     (select patient_id, visit_date, appointment_date\n"
-		        + "       from kenyaemr_etl.etl_prep_followup fup\n" + "      )\n" + "     union\n" + "     (\n"
-		        + "      select patient_id, visit_date, next_appointment as appointment_date\n"
-		        + "       from kenyaemr_etl.etl_prep_monthly_refill\n" + "       )\n" + "      ) t\n"
-		        + "    left outer JOIN\n" + "     (select patient_id, visit_date\n"
-		        + "       from kenyaemr_etl.etl_prep_discontinuation\n" + "       where date(visit_date) <= curdate()\n"
-		        + "       group by patient_id -- check if this line is necessary\n"
-		        + "      ) d on d.patient_id = t.patient_id\n" + "      group by t.patient_id\n" + " ) f\n"
+		String qry = "select  m.patient_id  from (\n"
+		        + "   select f.patient_id, latest_vis_date, latest_tca, date_discontinued, disc_patient    from (\n"
+		        + "   select t.patient_id,     max(t.visit_date) as visit_date,     mid(max(concat(t.visit_date, t.latest_tca)), 11) as latest_tca,\n"
+		        + "    max(t.visit_date) as latest_vis_date,     d.patient_id      as disc_patient,\n"
+		        + "    d.disc_date as date_discontinued     from (\n"
+		        + "     (select patient_id, max(visit_date) as visit_date, mid(max(concat(visit_date, appointment_date)), 11) as latest_tca\n"
+		        + "       from kenyaemr_etl.etl_prep_followup group by   patient_id  )      union      (\n"
+		        + "      select patient_id, max(visit_date), mid(max(concat(visit_date, next_appointment)), 11) as latest_tca\n"
+		        + "       from kenyaemr_etl.etl_prep_monthly_refill group by patient_id         )       ) t\n"
+		        + "    left outer JOIN      (select patient_id, max(visit_date) as disc_date\n"
+		        + "       from kenyaemr_etl.etl_prep_discontinuation        where date(visit_date) <= curdate()\n"
+		        + "       group by patient_id\n"
+		        + "      ) d on d.patient_id = t.patient_id       group by t.patient_id  ) f\n"
 		        + " JOIN kenyaemr_etl.etl_patient_demographics pd ON f.patient_id = pd.patient_id\n"
 		        + " JOIN kenyaemr_etl.etl_prep_enrolment e on f.patient_id = e.patient_id\n"
-		        + " where latest_vis_date <= curdate()\n" + " GROUP BY f.patient_id\n" + " having (\n"
-		        + "  (((date(latest_tca) < curdate()) and (date(latest_vis_date) < date(latest_tca))))\n"
+		        + " where latest_vis_date <= curdate()  GROUP BY f.patient_id  having (\n"
+		        + "  (((timestampdiff(DAY, date(latest_tca), date(curdate())) >= 7) and  (date(latest_vis_date) < date(latest_tca))))\n"
 		        + "   and ((date(latest_tca) > date(date_discontinued)\n"
-		        + "   and date(latest_vis_date) > date(date_discontinued)) or disc_patient is null)\n" + " )) m;";
+		        + "   and date(latest_vis_date) > date(date_discontinued)) or disc_patient is null)  )) m;\n";
 		
 		SqlQueryBuilder builder = new SqlQueryBuilder();
 		builder.append(qry);
--- api/src/main/java/org/openmrs/module/prep/reporting/data/converter/definition/evaluator/prep/NextAppointmentDateDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/prep/reporting/data/converter/definition/evaluator/prep/NextAppointmentDateDataEvaluator.java
@@ -36,22 +36,25 @@ public class NextAppointmentDateDataEvaluator implements PersonDataEvaluator {
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "select e.patient_id,\n"
-		        + "       if(greatest(coalesce(f.next_clinical_appointment,'0000-00-00'), coalesce(r.next_refill_appointment,'0000-00-00')) != '0000-00-00',\n"
-		        + "          greatest(coalesce(f.next_clinical_appointment,'0000-00-00'), coalesce(r.next_refill_appointment,'0000-00-00')), null) as appointment_date\n"
-		        + "       from kenyaemr_etl.etl_prep_enrolment e\n"
-		        + "                left join (select f.patient_id,\n"
-		        + "                                  mid(max(concat(date(f.visit_date), date(f.appointment_date))),\n"
-		        + "                                      11) as next_clinical_appointment\n"
-		        + "                           from kenyaemr_etl.etl_prep_followup f\n"
-		        + "                           where date(f.visit_date) <= date(:endDate)\n"
-		        + "                           group by f.patient_id) f on e.patient_id = f.patient_id\n"
-		        + "                left join (select r.patient_id,\n"
-		        + "                                  mid(max(concat(date(r.visit_date), date(next_appointment))), 11) as next_refill_appointment\n"
-		        + "                           from kenyaemr_etl.etl_prep_monthly_refill r\n"
-		        + "                           where date(r.visit_date) <= date(:endDate)\n"
-		        + "                           group by r.patient_id) r on e.patient_id = r.patient_id\n"
-		        + "       where date(e.visit_date) <= date(:endDate)group by e.patient_id;";
+		String qry = "select  m.patient_id,m.latest_tca  from (\n"
+		        + "         select f.patient_id, latest_vis_date, latest_tca, date_discontinued, disc_patient   from (\n"
+		        + "         select t.patient_id,    max(t.visit_date) as visit_date,    mid(max(concat(t.visit_date, t.latest_tca)), 11) as latest_tca,\n"
+		        + "          max(t.visit_date) as latest_vis_date,    d.patient_id      as disc_patient,\n"
+		        + "          d.disc_date as date_discontinued    from (\n"
+		        + "           (select patient_id, max(visit_date) as visit_date, mid(max(concat(visit_date, appointment_date)), 11) as latest_tca\n"
+		        + "             from kenyaemr_etl.etl_prep_followup group by   patient_id  )     union     (\n"
+		        + "            select patient_id, max(visit_date), mid(max(concat(visit_date, next_appointment)), 11) as latest_tca\n"
+		        + "             from kenyaemr_etl.etl_prep_monthly_refill group by patient_id         )      ) t\n"
+		        + "          left outer JOIN     (select patient_id, max(visit_date) as disc_date\n"
+		        + "             from kenyaemr_etl.etl_prep_discontinuation       where date(visit_date) <= curdate()\n"
+		        + "             group by patient_id\n"
+		        + "            ) d on d.patient_id = t.patient_id      group by t.patient_id ) f\n"
+		        + "       JOIN kenyaemr_etl.etl_patient_demographics pd ON f.patient_id = pd.patient_id\n"
+		        + "       JOIN kenyaemr_etl.etl_prep_enrolment e on f.patient_id = e.patient_id\n"
+		        + "       where latest_vis_date <= curdate() GROUP BY f.patient_id having (\n"
+		        + "        (((timestampdiff(DAY, date(latest_tca), date(curdate())) >= 7) and  (date(latest_vis_date) < date(latest_tca))))\n"
+		        + "         and ((date(latest_tca) > date(date_discontinued)\n"
+		        + "         and date(latest_vis_date) > date(date_discontinued)) or disc_patient is null) )) m;";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		queryBuilder.append(qry);
